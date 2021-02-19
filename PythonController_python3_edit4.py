fc11513cb09e0c6cccd77e388af2f36e0b25c006
Greg <49172966+Gregpy@users.noreply.github.com>
2021-02-19 11:57:56 -0500

    Update PythonController_python3_edit4.py

--- PythonController_python3_edit4.py
+++ PythonController_python3_edit4.py
@@ -4,17 +4,18 @@ Created on Wed Jul 04 09:18:21 2018
     
 @author: James Fraser
 
+And Greg 2020-21
 """
 #####============ IMPORT LIBRARIES =================####
 
-import ArduinoClassForTemp_python3_edit4 #changed for python3
-import TkinterClass_python3_edit4 as tkc #changed for python3
+import ArduinoClassForTemp_python3_edit4 # change if filename changes
+import TkinterClass_python3_edit4 as tkc # change if filename changes
 import tkinter as tk
-#from mttkinter import mtTkinter as tk #trying out
+
 
 #####============ OBJECT INITIALIZATION  ===========####
 
-ard = ArduinoClassForTemp_python3_edit4.Arduino(verbose = 1)    # Create Arduino object for serial communications, and for data storage functionality
+ard = ArduinoClassForTemp_python3_edit4.Arduino(verbose = 0)    # Create Arduino object for serial communications, and for data storage functionality
 master = tk.Tk()   # Create frame object, so that we may access methods for creating a GUI via widgets (labels, entries, and buttons)                                   
 ard.associate(master)     
 
@@ -22,12 +23,12 @@ ard.associate(master)
 #####============ INTERFACE ========================####
 # Create GUI using tk.Tk() object and with functionality from Arduino object
 
-GUI = tkc.Interface(master,ard, verbose = 1 ) 
+GUI = tkc.Interface(master,ard, verbose = 0) 
          
 
 #####============ THREAD ACTIVATION ================####
 # Start the internal thread for each object, which lets Python run multiple loops at once
 
 ard.thread.start()
-tk.mainloop() # replaces next line, only call mainloop once, in the main thread, shouldn't make another thread
-#GUI.thread.start()
+tk.mainloop() # only call mainloop once, in the main thread, shouldn't make another thread, changed for python 3
+
